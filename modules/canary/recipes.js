68df7795fc32e9f9a4113139ceae1ffff1063610
RyehJael <lostinncertainty@aim.com>
2015-05-22 00:52:14 -0400

    made recipes functional

--- modules/canary/recipes.js
+++ modules/canary/recipes.js
@@ -20,15 +20,55 @@ function createRecipe( recipe ){
       cells = recipe.shape[i].split('');
       rr = [];
       for ( j = 0; j < cells.length ; j++){
-	if (cells[j] != ' '){
-	  rr.push(recipe.ingredients[cells[j]]);
-	}
+      	if (cells[j] != ' '){
+      	  rr.push(recipe.ingredients[cells[j]]);
+      	}
       }
       rows.push( new cmRecipeRow(recipe.shape[i], rr) );
     }
-    result = cmRecipe.createShapedRecipe( recipe.result, rows);
-  } else { 
-    result = cmRecipe.createShapelessRecipe( recipe.result, recipe.ingredients );
+    // This line is erroring >> Corrected : changed rows to individual parameters
+    switch(rows.length){
+      case 1 :
+        result = cmRecipe.createShapedRecipe( recipe.result, rows[0]);
+        break
+      case 2 :
+        result = cmRecipe.createShapedRecipe( recipe.result, rows[0], rows[1]);
+        break
+      case 3 :
+        result = cmRecipe.createShapedRecipe( recipe.result, rows[0], rows[1], rows[2]);
+        break
+    }
+  } else {
+    var ri = recipe.ingredients
+    switch(ri.length){
+      case 1 :
+        result = cmRecipe.createShapelessRecipe( recipe.result, ri[0] );
+        break
+      case 2 :
+        result = cmRecipe.createShapelessRecipe( recipe.result, ri[0], ri[1] );
+        break
+      case 3 :
+        result = cmRecipe.createShapelessRecipe( recipe.result, ri[0], ri[1], ri[2] );
+        break
+      case 4 :
+        result = cmRecipe.createShapelessRecipe( recipe.result, ri[0], ri[1], ri[2], ri[3] );
+        break
+      case 5 :
+        result = cmRecipe.createShapelessRecipe( recipe.result, ri[0], ri[1], ri[2], ri[3], ri[4] );
+        break
+      case 6 :
+        result = cmRecipe.createShapelessRecipe( recipe.result, ri[0], ri[1], ri[2], ri[3], ri[4], ri[5] );
+        break
+      case 7 :
+        result = cmRecipe.createShapelessRecipe( recipe.result, ri[0], ri[1], ri[2], ri[3], ri[4], ri[5], ri[6] );
+        break
+      case 8 :
+        result = cmRecipe.createShapelessRecipe( recipe.result, ri[0], ri[1], ri[2], ri[3], ri[4], ri[5], ri[6], ri[7] );
+        break
+      case 9 :
+        result = cmRecipe.createShapelessRecipe( recipe.result, ri[0], ri[1], ri[2], ri[3], ri[4], ri[5], ri[6], ri[7], ri[8] );
+        break
+    }
   }
   return result;
 }
---
+++ plugins/crafting.js
@@ -0,0 +1,86 @@
+var items = require('items');
+
+// Fixed
+var recipes = require('recipes');
+
+// Used to bypass existing module
+var Recipes = Packages.net.canarymod.api.inventory.recipes.CraftingRecipe;
+var RecipeRow = Packages.net.canarymod.api.inventory.recipes.RecipeRow;
+
+
+
+// Creates a shapeless recipe
+var wand = items.stick(1)
+wand.setDisplayName('Wand')
+var shapelessRecipe = Recipes.createShapelessRecipe(wand,[items.stick(1),items.stick(1)])
+var addedshapelessRecipe = server.addRecipe(shapelessRecipe)
+console.log('Successfully created wand!')
+
+
+
+// Creates a shaped recipe
+var staff = items.stick(1)
+staff.setDisplayName('Staff')
+var rows = [
+	new RecipeRow("A",[items.stick(1)]),
+	new RecipeRow("A",[items.stick(1)]),
+	new RecipeRow("A",[items.stick(1)]),
+]
+var shapedRecipe = Recipes.createShapedRecipe(staff,rows[0],rows[1],rows[2])
+var addedShapedRecipe = server.addRecipe(shapedRecipe)
+console.log('Successfully created staff!')
+
+
+// Creates a shaped recipe using recipes module
+var enderBow = items.bow(1)
+enderBow.setDisplayName('Enderbow')
+var enderBowRecipe = {
+  result: enderBow,
+  ingredients: {
+    E: items.enderPearl(1),
+    S: items.stick(1),
+    W: items.string(1)
+  },
+  shape: [ 'ESW',
+           'SEW',
+           'ESW' ]
+}
+recipes.add( enderBowRecipe )
+console.log("Successfully created enderbow!")
+
+
+// Creates a shapeless recipe using recipes module
+var whip = items.fishingRod(1)
+whip.setDisplayName('Whip')
+var whipRecipe = {
+  result: whip,
+  ingredients: [items.stick(1),items.string(1)]
+}
+recipes.add( whipRecipe )
+console.log("Successfully created whip!")
+
+
+
+// Adding recipes to an object or array can make loading them as a group easier.
+var customRecipes = {}
+
+var flawlessEmerald = items.emerald(1)
+flawlessEmerald.setDisplayName('Flawless Emerald')
+customRecipes['Flawless Emerald'] = {
+	result: flawlessEmerald,
+	ingredients: [items.emeraldBlock(1),items.emeraldBlock(1),items.emeraldBlock(1),
+	items.emeraldBlock(1),items.emeraldBlock(1),items.emeraldBlock(1),
+	items.emeraldBlock(1),items.emeraldBlock(1),items.emeraldBlock(1)]
+}
+
+function addRecipes(recipesArray){
+	for (var i in recipesArray){
+		if (recipesArray[i].result){
+			recipes.add(recipesArray[i])
+			console.log('Added recipe : ' + recipesArray[i].result.getDisplayName())
+		}
+
+	}
+}
+
+addRecipes(customRecipes)
